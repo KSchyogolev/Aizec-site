c86049b3ff06c1fe27f121be9b405794aede6d21
shchegolev <shchegolev@speechpro.com>
2019-06-26 15:43:30 +0300

    Added users api

--- webapp/src/components/pages/users.page.js
+++ webapp/src/components/pages/users.page.js
@@ -2,48 +2,48 @@ import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { inject, observer } from 'mobx-react'
 import store from '../../store'
-import MaterialTable from "material-table";
-import { forwardRef } from 'react';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowUpward from '@material-ui/icons/ArrowUpward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
+import MaterialTable from "material-table"
+import { forwardRef } from 'react'
+import AddBox from '@material-ui/icons/AddBox'
+import ArrowUpward from '@material-ui/icons/ArrowUpward'
+import Check from '@material-ui/icons/Check'
+import ChevronLeft from '@material-ui/icons/ChevronLeft'
+import ChevronRight from '@material-ui/icons/ChevronRight'
+import Clear from '@material-ui/icons/Clear'
+import DeleteOutline from '@material-ui/icons/DeleteOutline'
+import Edit from '@material-ui/icons/Edit'
+import FilterList from '@material-ui/icons/FilterList'
+import FirstPage from '@material-ui/icons/FirstPage'
+import LastPage from '@material-ui/icons/LastPage'
+import Remove from '@material-ui/icons/Remove'
+import SaveAlt from '@material-ui/icons/SaveAlt'
+import Search from '@material-ui/icons/Search'
+import ViewColumn from '@material-ui/icons/ViewColumn'
 
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref}/>),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+  SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref}/>),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+}
 
 const styles = theme => ({
   root: {
     maxWidth: '100%',
-    margin:'15px'
+    margin: '15px'
   }
 })
 
@@ -51,7 +51,7 @@ const styles = theme => ({
 @observer
 class UsersPage extends Component {
 
-  componentDidMount(){
+  componentDidMount () {
     this.props.store.getUsers()
   }
 
@@ -63,54 +63,30 @@ class UsersPage extends Component {
           title="Пользователи"
           icons={tableIcons}
           columns={[
-            { title: "Adı", field: "name" },
-            { title: "Soyadı", field: "surname" },
-            { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+            {title: "Логин", field: "login"},
+            {title: "Имя", field: "first_name"},
+            {title: "Фамилия", field: "second_name"},
+            {title: "Пароль", field: "password"},
+            {title: "Почта", field: "email"},
+            {title: "Телефон", field: "Phone"},
+            {title: "Информация", field: "bio"},
             {
-              title: "Doğum Yeri",
-              field: "birthCity",
-              lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
+              title: "Роль",
+              field: "role",
+              lookup: {'admin': "Администрато", 'teacher': "Учитель", 'student': 'Ученик'}
             }
           ]}
-          data={[
-            { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 }
-          ]}
+          data={store.users}
           editable={{
-            onRowAdd: newData =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-/*                    const data = this.state.data;
-                    data.push(newData);
-                    this.setState({ data }, () => resolve());*/
-                  }
-                  resolve()
-                }, 1000)
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-/*                    const data = this.state.data;
-                    const index = data.indexOf(oldData);
-                    data[index] = newData;
-                    this.setState({ data }, () => resolve());*/
-                  }
-                  resolve()
-                }, 1000)
-              }),
-            onRowDelete: oldData =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-/*                    let data = this.state.data;
-                    const index = data.indexOf(oldData);
-                    data.splice(index, 1);
-                    this.setState({ data }, () => resolve());*/
-                  }
-                  resolve()
-                }, 1000)
-              }),
+            onRowAdd: newData => new Promise((resolve, reject) => store.addUser(newData).then(resolve).catch(reject)),
+            onRowUpdate: (newData, oldData) => new Promise((resolve, reject) => store.updateUser(oldData.id, newData).then(() => {
+              this.forceUpdate()
+              resolve()
+            }).catch(reject)),
+            onRowDelete: oldData => new Promise((resolve, reject) => store.deleteUser(oldData.id).then(() => {
+              this.forceUpdate()
+              resolve()
+            }).catch(reject))
           }}
         />
       </div>
--- webapp/src/store.js
+++ webapp/src/store.js
@@ -2,13 +2,146 @@ import { action, observable } from 'mobx'
 import { RouterStore } from 'mobx-router'
 import API from './api/api'
 
+const users = [{
+  id: '1',
+  login: "admin2",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}, {
+  id: '2',
+  login: "admin2",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}, {
+  id: '3',
+  login: "admin2",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}, {
+  id: '4',
+  login: "admin2",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}, {
+  id: '5',
+  login: "admin2",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}]
+
+const user = {
+  id: '6',
+  login: "22333",
+  password: "123456",
+  password_confirmation: "123456",
+  first_name: "a",
+  second_name: "b",
+  role: "admin",
+  bio: "123",
+  phone: "1",
+  email: "aa@d.c"
+}
+
 class Store {
 
   @observable router = new RouterStore()
+  @observable users = []
+  @observable currentUser = {}
 
   @action
   getUsers () {
-    API.main.getUsers().then(res => console.log(res))
+    API.main.getAllUsers().then(res => this.users = res)
+    // this.users = users
+  }
+
+  @action
+  getUser (userId) {
+    API.main.getUser(userId).then(res => this.currentUser = res)
+    // this.currentUser = user
+  }
+
+  @action
+  addUser (data) {
+    return new Promise((resolve, reject) => {
+      API.main.addUser(data).then(res => {
+        this.users = [...this.users, res]
+        resolve()
+      }).catch(reject)
+      /*      setTimeout(() => {
+              this.users = [...this.users, data]
+              resolve()
+            }, 1000)*/
+    })
+  }
+
+  @action
+  updateUser (userId, data) {
+    return new Promise((resolve, reject) => {
+      API.main.updateUser(userId, data).then(res => {
+        const currentUsers = this.users
+        const index = currentUsers.findIndex(user => user.id === userId)
+        currentUsers[index] = res
+        this.users = currentUsers
+        resolve()
+      }).catch(reject)
+      /*      setTimeout(() => {
+              const currentUsers = this.users
+              const index = currentUsers.findIndex(user => user.id === userId)
+              currentUsers[index] = data
+              this.users = currentUsers
+              resolve()
+            }, 1000)*/
+    })
+  }
+
+  @action
+  deleteUser (userId) {
+    return new Promise((resolve, reject) => {
+      API.main.deleteUser(userId).then(res => {
+        const currentUsers = this.users
+        const index = currentUsers.findIndex(user => user.id === userId)
+        currentUsers.splice(index, 1)
+        this.users = currentUsers
+        resolve()
+      }).catch(reject)
+      /*      setTimeout(() => {
+              const currentUsers = this.users
+              const index = currentUsers.findIndex(user => user.id === userId)
+              currentUsers.splice(index, 1)
+              this.users = currentUsers
+              resolve()
+            }, 1000)*/
+    })
   }
 }
 
