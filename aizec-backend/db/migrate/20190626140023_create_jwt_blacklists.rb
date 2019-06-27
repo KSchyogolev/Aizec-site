892876892c03418e0d280dbce072abd36dd239f3
vlastachu <vlastachu@gmail.com>
2019-06-27 15:01:15 +0300

    fix class name

--- aizec-backend/db/migrate/20190626140023_create_jwt_blacklists.rb
+++ aizec-backend/db/migrate/20190626140023_create_jwt_blacklists.rb
@@ -1,4 +1,4 @@
-class CreateJwtBlacklist < ActiveRecord::Migration[5.2]
+class CreateJwtBlacklists < ActiveRecord::Migration[5.2]
   def change
     create_table :jwt_blacklist do |t|
       t.string :jti, null: false
