01ca1e6870ca2837bb91c53f8d9dc79f866af903
vlastachu <vlastachu@gmail.com>
2019-06-28 18:29:43 +0300

    use login for authentication

--- aizec-backend/app/controllers/application_controller.rb
+++ aizec-backend/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  respond_to :json
   def render_resource(resource)
     if resource.errors.empty?
       render json: resource
--- aizec-backend/app/controllers/sessions_controller.rb
+++ aizec-backend/app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
+  wrap_parameters :user, include: [:password, :password_confirmation, :email]
 
   private
 
