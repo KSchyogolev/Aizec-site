1aec8c69cc5554be8ed4b93883fb9b7d96b64743
vlastachu <vlastachu@gmail.com>
2019-09-08 18:25:30 +0300

    default nulls for visit

--- aizec-backend/app/models/visit.rb
+++ aizec-backend/app/models/visit.rb
@@ -2,9 +2,15 @@ class Visit < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
   
-  has_status %w[ok skip_without_reason skip_not_approved skip_approved_teacher skip_approved]
-  validates :approve_status, inclusion: { in: %w[done_not_approved done_approved need_fix], message: "%{value} is not valid homework status." }
-
+  has_status %w[null ok skip_without_reason skip_not_approved skip_approved_teacher skip_approved]
+  validates :approve_status, inclusion: { in: %w[null done_not_approved done_approved need_fix], message: "%{value} is not valid homework status." }
+  before_save :default_values
+  
   include Receivable
 
+  private 
+  def default_values
+    status ||= 'null' 
+    approve_status ||= 'null'
+  end
 end
