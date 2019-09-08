59a3c49a4bc11ae53c18b92bd3e28388dc19fc10
vlastachu <vlastachu@gmail.com>
2019-09-08 20:11:36 +0300

    before_validation

--- aizec-backend/app/models/visit.rb
+++ aizec-backend/app/models/visit.rb
@@ -4,7 +4,7 @@ class Visit < ApplicationRecord
   
   has_status %w[null ok skip_without_reason skip_not_approved skip_approved_teacher skip_approved]
   validates :approve_status, inclusion: { in: %w[null done_not_approved done_approved need_fix], message: "%{value} is not valid homework status." }
-  before_create :default_values
+  before_validation :default_values
   
   include Receivable
 
