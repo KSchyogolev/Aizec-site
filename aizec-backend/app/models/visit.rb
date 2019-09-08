878082da91e223003635b55bd07d07cd0f979d6e
vlastachu <vlastachu@gmail.com>
2019-09-08 20:13:46 +0300

    after_initialize

--- aizec-backend/app/models/visit.rb
+++ aizec-backend/app/models/visit.rb
@@ -4,7 +4,7 @@ class Visit < ApplicationRecord
   
   has_status %w[null ok skip_without_reason skip_not_approved skip_approved_teacher skip_approved]
   validates :approve_status, inclusion: { in: %w[null done_not_approved done_approved need_fix], message: "%{value} is not valid homework status." }
-  before_validation :default_values
+  after_initialize :default_values
   
   include Receivable
 
