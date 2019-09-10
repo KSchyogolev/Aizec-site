2253e0b347c9ce5e34f93ae1407617c606e236c7
vlastachu <vlastachu@gmail.com>
2019-09-10 20:31:23 +0300

    payment optional fields

--- aizec-backend/app/models/payment.rb
+++ aizec-backend/app/models/payment.rb
@@ -1,10 +1,11 @@
 class Payment < ApplicationRecord
   belongs_to :user
-  belongs_to :course
-  belongs_to :merch
+  belongs_to :course, optional: true
+  belongs_to :merch, optional: true
   validates :cost, :numericality => { greater_than_or_equal_to: 0, only_integer: true }
   validates :bonuses, :numericality => { greater_than_or_equal_to: 0, only_integer: true }
   has_status %w[archived ready done]
+
   
   include Archivable
 
