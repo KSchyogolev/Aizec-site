6dfb236ba6763f88ece49e17a713b28f1c005e91
vlastachu <vlastachu@gmail.com>
2019-09-18 19:45:50 +0300

    done_not_approved status on each send homeork

--- aizec-backend/app/controllers/messages_controller.rb
+++ aizec-backend/app/controllers/messages_controller.rb
@@ -22,7 +22,7 @@ class MessagesController < ApplicationController
     if @message.save
       if @message.kind == "homework" and @message.to_entity_type == "visit"
         visit = Visit.find(@message.to_entity_id)
-        if visit.present? and visit.approve_status == "null"
+        if visit.present?
           visit.approve_status = "done_not_approved"
           visit.save
         end
