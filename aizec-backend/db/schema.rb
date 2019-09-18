c62222302b4c396fa84a081ba33a49e2cb7e9f40
vlastachu <vlastachu@gmail.com>
2019-09-18 19:51:41 +0300

    add level to user

--- aizec-backend/db/schema.rb
+++ aizec-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_133133) do
+ActiveRecord::Schema.define(version: 2019_09_18_164711) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -216,6 +216,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_133133) do
     t.string "address"
     t.string "parents"
     t.date "birthday"
+    t.integer "level"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
