473e16b8f9ed90794f913bfcf03aa98a987343e4
vlastachu <vlastachu@gmail.com>
2019-09-20 14:23:23 +0300

    weee

--- aizec-backend/app/controllers/users_controller.rb
+++ aizec-backend/app/controllers/users_controller.rb
@@ -49,8 +49,7 @@ class UsersController < ApplicationController
   end
 
   def revoke_password
-    email = params.require(:user).permit(:email)
-    @user = User.find_by(email: email)
+    @user = User.find_by(params.require(:user).permit(:email))
     generated_password = Devise.friendly_token.first(10)
     @user.password = generated_password
     
