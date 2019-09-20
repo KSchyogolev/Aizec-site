4fa158552a37004a3fd2ac1bbc9aa9cf182ab470
vlastachu <vlastachu@gmail.com>
2019-09-20 16:20:06 +0300

    user render

--- aizec-backend/app/controllers/sessions_controller.rb
+++ aizec-backend/app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     #render json: resource
+    @user = resource
+    render :template => "users/show", formats: [:json]
   end
 
   def respond_to_on_destroy
