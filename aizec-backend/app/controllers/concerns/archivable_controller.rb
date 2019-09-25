d5bd0a566993251cf08257e53cc1bdd13493ab06
vlastachu <vlastachu@gmail.com>
2019-09-25 15:05:45 +0300

    remove allow admin

--- aizec-backend/app/controllers/concerns/archivable_controller.rb
+++ aizec-backend/app/controllers/concerns/archivable_controller.rb
@@ -5,8 +5,6 @@ module ArchivableController extend ActiveSupport::Concern
   # end
 
   def archivated_index
-    allow_admin
-    return if performed?
     resource_model = controller_name.classify.constantize
     plural_var = controller_name
     instance_variable_set("@#{plural_var}", resource_model.unscoped.where(status: "archived"))
@@ -14,8 +12,6 @@ module ArchivableController extend ActiveSupport::Concern
   end
 
   def with_archivated_index
-    allow_admin
-    return if performed?
     resource_model = controller_name.classify.constantize
     plural_var = controller_name
     instance_variable_set("@#{plural_var}", resource_model.unscoped.all)
@@ -23,8 +19,6 @@ module ArchivableController extend ActiveSupport::Concern
   end
 
   def archivated_show
-    allow_admin
-    return if performed?
     resource_model = controller_name.classify.constantize
     singular_var = controller_name.singularize
     instance_variable_set("@#{singular_var}", resource_model.unscoped.find(params[:id]))
