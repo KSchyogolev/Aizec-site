6d9e0536753c4e1b011ec8712a7c3a999089b4d4
vlastachu <vlastachu@gmail.com>
2019-09-25 15:07:15 +0300

    fixie

--- aizec-backend/app/controllers/concerns/archivable_controller.rb
+++ aizec-backend/app/controllers/concerns/archivable_controller.rb
@@ -8,20 +8,20 @@ module ArchivableController extend ActiveSupport::Concern
     resource_model = controller_name.classify.constantize
     plural_var = controller_name
     instance_variable_set("@#{plural_var}", resource_model.unscoped.where(status: "archived"))
-    render :template => "@#{plural_var}/index", formats: [:json]
+    render :template => "#{plural_var}/index", formats: [:json]
   end
 
   def with_archivated_index
     resource_model = controller_name.classify.constantize
     plural_var = controller_name
     instance_variable_set("@#{plural_var}", resource_model.unscoped.all)
-    render :template => "@#{plural_var}/index", formats: [:json]
+    render :template => "#{plural_var}/index", formats: [:json]
   end
 
   def archivated_show
     resource_model = controller_name.classify.constantize
     singular_var = controller_name.singularize
     instance_variable_set("@#{singular_var}", resource_model.unscoped.find(params[:id]))
-    render :template => "@#{plural_var}/show", formats: [:json]
+    render :template => "#{plural_var}/show", formats: [:json]
   end
 end
